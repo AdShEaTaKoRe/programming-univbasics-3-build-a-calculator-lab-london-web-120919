6c5981667d655b3e4d2135e3dad6942034ed498a
Tommy Berman <tomy.berman1@gmail.com>
2019-11-10 19:06:38 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,24 +1,24 @@
 def addition(num1, num2)
-  num1 + num2
+  puts num1 + num2
 end
 
 def subtraction(num1, num2)
-  num1 - num2
+  puts num1 - num2
 end
 
 def division(num1, num2)
-  num1\num2
+  puts num1\num2
 end
 
 def multiplication(num1, num2)
-  num1 * num2
+  puts num1 * num2
 end
 
 def modulo(num1, num2)
   sum = num1\num2
-  num1 - (sum * num2)
+  puts num1 - (sum * num2)
 end
 
 def square_root(num)
-  math.sqrt(num)
+  puts math.sqrt(num)
 end
